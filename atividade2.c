cda0bdd90b959f0bdaab90e4cd4fd4f1a8734d3f
jorgearaujo81 <jorge.araujo81@gmail.com>
2022-03-07 13:28:34 -0300

    Update commit

--- atividade2.c
+++ atividade2.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define QTD_MESSAGES 2
 
 int main() {
 
-	pid_t pid;
-	pid = fork();
-	int pipeFileDescriptors[2];
+    pid_t pid;
+    pid = fork();
+    int pipeFileDescriptors[2];
     int returnStatus;
     char writeMessages[QTD_MESSAGES][20] = {"Maria", "Joao andou a noite"};
     char readMessages[20];
@@ -20,19 +24,21 @@ int main() {
         printf("Error when create pipe\n");
 
         return 1;
-    }else{
-        if (pid > 0) {
-            for (int i = 0; i < QTD_MESSAGES; i++) {
-                printf("Writing Message %d is %s\n", i, writeMessages[i]);
-                write(pipeFileDescriptors[1], writeMessages[0], sizeof(char) * 20);
-                sleep(13);
-            }
-        }else {
-            for (int i = 0; i < QTD_MESSAGES; i++) {
-                read(pipeFileDescriptors[0], readMessages, sizeof(char) * 20);
-                printf("Reading Message %d is %s\n", i, readMessages);
-            }
+    }
+
+    for (int i = 0; i < QTD_MESSAGES; i++) {
+        if (pid != 0){
+            printf("Writing Message %d is %s\n", i, writeMessages[i]);
+            write(pipeFileDescriptors[1], writeMessages[0], sizeof(char) * 20);
+            read(pipeFileDescriptors[0], readMessages, sizeof(char) * 20);
+            printf("Reading Message %d is %s\n", i, readMessages);
+        }else{
+            printf("Writing Message %d is %s\n", i, writeMessages[i]);
+            write(pipeFileDescriptors[1], writeMessages[0], sizeof(char) * 20);
+            read(pipeFileDescriptors[0], readMessages, sizeof(char) * 20);
+            printf("Reading Message %d is %s\n", i, readMessages);
         }
     }
-	return 0;
+
+    return 0;
 }
